2b0687de1ac1976d8d44708ac102b5012ff8686b
Sebastin Raj <134362610+sebastin6000@users.noreply.github.com>
2024-11-18 18:29:11 +0530

    Update 05_HexadecimalToBinary.java

--- 05_HexadecimalToBinary.java
+++ 05_HexadecimalToBinary.java
@@ -82,7 +82,7 @@ public class HexadecimalToBinary {
 
     // Hexadecimal-to-binary mapping manually
     String[] hexToBinaryMap = {
-      "0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"
+      "0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011", "1100","1101","1110","1111"
     };
 
     // Step 2: For each character in the hexadecimal string
