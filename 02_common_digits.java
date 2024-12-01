24ab210d79f78e19248878861b0deff02f25f2b7
Sebastin Raj <sebastinraj070@gmail.com>
2024-12-01 09:45:22 +0530

    Update 02_common_digits.java

--- 02_common_digits.java
+++ 02_common_digits.java
@@ -1,19 +1,28 @@
 
 /* 
 Section-1: Question Section
-Question:
 Given an array nums of positive integers of size N. Find all distinct digits present in nums.
-
-Examples:
+ 
+Example 1:
 Input: nums = [131, 11, 48]
 Output: 1 3 4 8
-Explanation: The digits 1, 3, 4, and 8 are distinct in the array.
+Explanation: 1, 3, 4, and 8 are only distinct digits that can be extracted from the numbers of the array.
 
+Example 2:
 Input: nums = [111, 222, 333, 4444, 666]
 Output: 1 2 3 4 6
-Explanation: The digits 1, 2, 3, 4, and 6 are distinct in the array.
-
-Your task is to implement the function common_digits() which takes nums as input parameter and returns a list of digits which can be extracted from the given array in ascending order.
+Explanation: 1, 2, 3, 4, and 6 are only distinct digits that can be extracted from the numbers of the array.
+ 
+Your Task: 
+You don't need to read or print anything. 
+Your task is to complete the function common_digits() which takes nums as input parameter and returns a list of digits which can be extracted from the given array in ascending order.
+ 
+Expected Time Complexity: O(N)
+Expected Space Complexity: O(N)
+ 
+Constraints:
+1 <= N <= 105
+1 <= numsi <= 10.
 */
 
 /* 
